28cad3aca3a0bfa07aaa3949a3919b25f1f3a4ca
William <william.tigoias@gmail.com>
2025-01-31 18:06:54 -0300

    Updates

--- src/components/Picture.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Picture.css';
-
-export default function Picture(props){
-    return(
-        <img className="picture" src={props.img} alt={props.nomeImg}/>
-    );
-}
